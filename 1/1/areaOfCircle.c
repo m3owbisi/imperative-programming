b0b3c6a516fd6850b113e99f0a262f8b992b99ff
m3owbisi <146631342+m3owbisi@users.noreply.github.com>
2024-05-21 17:43:38 +0530

    Delete 1/1 directory

--- 1/1/areaOfCircle.c
+++
@@ -1,9 +0,0 @@
-//area of a circle
-#include<stdio.h>
-int main(){
-    float radius;
-    printf("enter radius:\t");
-    scanf("%f",&radius);
-    printf("area of a circle is:\t%f",3.14*radius*radius);
-    return 0;
-}
--- 1/1/areaOfSquare.c
+++
@@ -1,9 +0,0 @@
-//area of a square
-#include<stdio.h>
-int main(){
-    int side;
-    printf("enter side:\t");
-    scanf("%d",&side);
-    printf("area of a square is:\t%d",side*side);
-    return 0;
-}
